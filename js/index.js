245c94da27975e6584fcd75343894b923c447a44
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-02-19 13:42:42 +0100

    19-02-2024 /v2

--- js/index.js
+++ js/index.js
@@ -9,59 +9,54 @@ function setLocalStorage(key, value) {
 
 // fonction pour recuperer les elements dans  le local storage
 function getLocalStorage(key) {
-    return JSON.parse(localStorage.getItem(key));
+    if (localStorage.getItem(key) == null) {
+        return 0 ;
+    }
+    else{
+        return JSON.parse(localStorage.getItem(key));
+    }
 }
 
 
 // foction pour supprimer un local storage
 localStorage.removeItem('myValue');
 localStorage.clear();
+// setLocalStorage('root', "root")
 
-// function init() {
-    
-// // initialisation de la valeur de depart du panier
-// if (localStorage.getItem('current_value') !== null) {
-//     // L'élément existe dans le local storage
-//     document.getElementById("count").innerHTML= getLocalStorage('current_value');
-// } else {
-//     // L'élément n'existe pas dans le local storage
-//     setLocalStorage('current_value', 0);
-//     document.getElementById("count").innerHTML= getLocalStorage('current_value');
-// }
-// }
 
 
 function init() {
-    // initialisation de la valeur de départ du panier
-    if (localStorage.getItem('current_value') === null || document.readyState === 'loading') {
-      // L'élément n'existe pas dans le local storage ou la page n'est pas encore chargée complètement
-      setLocalStorage('current_value', 0);
-    }
+    document.getElementById("count").innerHTML= getLocalStorage('current_value');
+}
+
+
+// function init() {
+//     // initialisation de la valeur de départ du panier
+//     if (localStorage.getItem('current_value') === null || document.readyState === 'loading') {
+//       // L'élément n'existe pas dans le local storage ou la page n'est pas encore chargée complètement
+//       setLocalStorage('current_value', 0);
+//     }
   
-    let currentValue = getLocalStorage('current_value');
-    document.getElementById("count").innerHTML = currentValue;
-  }
+//     let currentValue = getLocalStorage('current_value');
+//     document.getElementById("count").innerHTML = currentValue;
+// }
   
 
 
 function add() {
-    
-      
-    //recuperation de l'element panier
-    current_value = parseInt(getLocalStorage('current_value'));
 
-    current_value +=1;
+        // L'élément existe dans le local storage
+        // document.getElementById("count").innerHTML= getLocalStorage('current_value');
+        current_value = parseInt(getLocalStorage('current_value'));
 
-    setLocalStorage('current_value', current_value);
-
-
-
+        //recuperation de l'element panier
     
 
-    
-    
+        current_value +=1;
 
-    //affectation de la nouvelle valeur 
-    document.getElementById("count").innerHTML= getLocalStorage('current_value');
+        setLocalStorage('current_value', current_value);
+
+        //affectation de la nouvelle valeur 
+        document.getElementById("count").innerHTML= getLocalStorage('current_value');
 
 }
